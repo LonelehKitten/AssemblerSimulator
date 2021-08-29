1c1aede48bc6cec557bb60c4bc84c1c22652b179
LonelehKitten <scarface.theminer@gmail.com>
2021-08-29 19:05:58 -0300

    project setup

---
+++ public/main.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow} = require('electron');
+
+require('@electron/remote/main').initialize();
+
+function createWindow() {
+    // Create the browser window.
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            enableRemoteModule: true,
+        }
+    });
+    window.loadURL('http://localhost:3000');
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    // On OS X it is common for application and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+})
