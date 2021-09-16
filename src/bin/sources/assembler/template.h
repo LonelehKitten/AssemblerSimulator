8d504e7e1ff8769db0a14ce0b8011079dc4ff22b
KDOXG <kevin.20pereira@gmail.com>
2021-09-16 13:50:20 -0300

    macro init
    Co-authored-by: Juliano-hub <Juliano-hub@users.noreply.github.com>

--- src/bin/sources/assembler/template.h
+++ src/bin/sources/assembler/template.h
@@ -1,3 +1,28 @@
 #include "Semantic.h"
 #include "SyntaxAnalyzer.h"
 
+#define T std::string
+
+class Macro
+{
+private:
+    std::string name;   // nome de chamada da macro
+    std::vector<T> args; // #00 #01 ...
+    std::string macroText; // texto da definição da macro
+    
+public:
+    // constructor
+    Macro();
+
+    // getters
+    std::string getName();
+    std::vector<T> getArgs();
+    std::string getText();
+
+    // métodos
+    std::string macroLine();
+    void updateMacro(std::vector<T> args);
+
+};
+
+std::vector<Macro> table;
---
+++ src/bin/sources/assembler/test.cpp
@@ -0,0 +1,8 @@
+#include "template.h"
+
+int main(int argc, char **argv){
+    Macro MacroMain;
+
+};
+
+// mover esse arquivo para uma pasta chamada 'test' dentro do arquivo bin depois
