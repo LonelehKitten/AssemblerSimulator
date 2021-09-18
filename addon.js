706480e35f35cf88138e60d91df1deb4a7e9b15c
LonelehKitten <scarface.theminer@gmail.com>
2021-09-18 17:54:08 -0300

    comecando a integrar

--- addon.js
+++ addon.js
@@ -1,3 +1,28 @@
+'use strict'
+
+const EventEmitter = require('events').EventEmitter
+const addon = require('bindings')('addon.node')
+
+const emitter = new EventEmitter()
+
+emitter.on('start', () => {
+    console.log('### START ...')
+})
+emitter.on('data', (evt) => {
+    console.log(evt);
+})
+
+emitter.on('end', () => {
+    console.log('### END ###')
+})
+
+addon.callEmit(emitter.emit.bind(emitter))
+
+
+
+/*
 var addon = require('bindings')('addon.node')
 
 console.log('This should be eight:', addon.add(3, 5))
+
+*/
