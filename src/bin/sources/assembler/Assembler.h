a8edf12d2e5c9276bcb7e36791eaad9dd8140190
KDOXG <kevin.20pereira@gmail.com>
2021-09-18 17:42:07 -0300

    update file

--- src/bin/sources/assembler/Assembler.h
+++ src/bin/sources/assembler/Assembler.h
@@ -5,11 +5,7 @@
 #include <iostream>
 #include <string>
 #include "../analyzer/Semantic.h"
-<<<<<<< HEAD
 #include "macroDef.h"
-=======
-#include "template.h"
->>>>>>> 19d9fba70ef35e0a348157f8da31e081225bb791
 
 class Assembler
 {
