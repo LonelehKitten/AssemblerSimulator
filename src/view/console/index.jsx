cccc7dc499ef545293270439a1caff543854b4dd
fernandOlle <frolle@inf.ufpel.edu.br>
2021-09-18 00:43:56 -0300

    styling consolo

--- src/view/console/index.jsx
+++ src/view/console/index.jsx
@@ -1,54 +1,91 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
 const Console = (props) => {
+  const useStyles = makeStyles((theme) => ({
+    '@global': {
+      '*::-webkit-scrollbar': {
+        width: '0.4em',
+        height: '0.4em',
+      },
+      '*::-webkit-scrollbar-track': {
+        '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.02)',
+      },
+      '*::-webkit-scrollbar-thumb': {
+        backgroundColor: '#191b24',
+        outline: '1px solid ',
+      },
+    },
+    textField: {
+      fontFamily: 'VT323',
+      marginBottom: '5px',
+      backgroundColor: 'transparent',
+      border: `3px inset ${theme.palette.primary.main}`,
+      padding: '10px',
+      color: '#fff',
+      backgroundColor: '#282a36',
+      fontSize: 24,
+      outline: 0,
+      borderRadius: '3px',
+    },
+    inputedTexts: {
+      backgroundColor: '#44475a',
+      borderRadius: '20px 0px 0px 20px',
+      minHeight: '30px',
+      marginTop: '5px',
+    },
+  }));
 
-    const useStyles = makeStyles((theme) => ({
-        textField: {
-            fontFamily: "VT323",
-            marginBottom: "5px",
-            backgroundColor:"transparent",
-            border: `3px inset ${theme.palette.primary.main}`,
-            padding: "10px",
-            color:"#fff",
-            backgroundColor: "#282a36",
-            fontSize:24,
-            outline:0,
-            borderRadius: "3px",
-        },
-        inputedTexts: {
-            padding: "0.2em", 
-            backgroundColor: "#646b91", 
-            margin: "0.2em",
-            borderRadius: "20px",
-            width: "max-content"
-        }
-    }));
+  const [history, setHistory] = useState([]);
 
-    const [history, setHistory] = useState([]);
-    
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const handleSubmit = (e) => {
-        if(e.keyCode == 13){
-            const value = e.target.value;
-            if(value == "") return;
-            setHistory((oldValue) => [...oldValue,value]);
-            e.target.value = "";
-        }
+  const handleSubmit = (e) => {
+    if (e.keyCode == 13) {
+      const value = e.target.value;
+      if (value == '') return;
+      setHistory((oldValue) => [...oldValue, value]);
+      e.target.value = '';
     }
- 
-    return (
-        <div id="console" {...props} >
-            {history.map((item) => <div className={classes.inputedTexts}>
-                {"> "+item}
-                </div>)
-            }
-            <br />
-            <input onKeyDown={handleSubmit} placeholder="Digite um comando . . ." className={classes.textField} />
-        </div>
-    )
-}
+  };
+
+  return (
+    <div id='console' {...props}>
+      <List
+        sx={{
+          position: 'relative',
+          overflow: 'auto',
+          '&::-webkit-scrollbar': {
+            width: '0.3em',
+            height: '0.4em',
+          },
+          '&::-webkit-scrollbar-track': {
+            '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.5)',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: '#191b24',
+            outline: '0px solid ',
+          },
+        }}
+      >
+        {history.map((item) => (
+          <ListItem className={classes.inputedTexts}>
+            <ListItemText primary={`> ⠀ ${item}`} />
+          </ListItem>
+        ))}
+      </List>
+      <input
+        onKeyDown={handleSubmit}
+        placeholder='Digite um comando . . .'
+        className={classes.textField}
+      />
+    </div>
+  );
+};
 
 export default Console;
