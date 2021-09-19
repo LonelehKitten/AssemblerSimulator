5a1915a3ece99696a68bbfeca452140fcf72671f
fernandOlle <frolle@inf.ufpel.edu.br>
2021-09-18 22:58:06 -0300

    formatting files

--- src/view/editor/index.jsx
+++ src/view/editor/index.jsx
@@ -1,22 +1,24 @@
-import React,{useState} from "react";
-import { useContext } from "../../utils/context";
+import React, { useState } from 'react';
+import { useContext } from '../../utils/context';
 
 import Ace from './ace';
 import Tabs from './tabs';
 
 const Editor = (props) => {
-    const {listFiles,changeFile} = useContext();
-    const [value, setValue] = useState("");
+  const { listFiles, changeFile } = useContext();
+  const [value, setValue] = useState('');
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-        changeFile(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    changeFile(newValue);
+  };
 
-    return <div id="editor" {...props}>
-        <Tabs value={value} onChange={handleChange} listFiles={listFiles} />
-        <Ace onChange={() => console.log("aa")} />
-    </div>;
-}
+  return (
+    <div id='editor' {...props}>
+      <Tabs value={value} onChange={handleChange} listFiles={listFiles} />
+      <Ace onChange={() => console.log('aa')} />
+    </div>
+  );
+};
 
 export default Editor;
--- src/view/editor/tabs.jsx
+++ src/view/editor/tabs.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState } from 'react';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import { makeStyles } from '@material-ui/core/styles';
@@ -7,109 +7,120 @@ import Tab from '@material-ui/core/Tab';
 
 import AddIcon from '@material-ui/icons/Add';
 
-
-import { useContext } from "../../utils/context";
+import { useContext } from '../../utils/context';
 
 import Item from './item';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        
-        backgroundColor: '#222534 ',
-        color: '#fff',
-        minHeight: '2rem',
-        "& 	.MuiTab-root": {
-            padding: '0 12px',
-            minHeight: '2rem'
-        }
+  root: {
+    backgroundColor: '#222534 ',
+    color: '#fff',
+    minHeight: '2rem',
+    '& 	.MuiTab-root': {
+      padding: '0 12px',
+      minHeight: '2rem',
     },
-    plus: {
-        minWidth: "auto"
-    }
+  },
+  plus: {
+    minWidth: 'auto',
+  },
 }));
 
-const {ipcRenderer} = window.electron;
+const { ipcRenderer } = window.electron;
 
 const Tabs = ({ value, onChange, listFiles }) => {
-    const { addFile, currentID, alertShow } = useContext();
-    const classes = useStyles();
+  const { addFile, currentID, alertShow } = useContext();
+  const classes = useStyles();
 
-    const inputFile = useRef(null)
-    const [anchorEl, setAnchorEl] = useState(null);
+  const inputFile = useRef(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-    const onChangeFile = (event) => {
-        event.stopPropagation();
-        event.preventDefault();
-        const file = event.target.files[0];
-        const reader = new FileReader();
-        reader.readAsText(file, "UTF-8");
-        reader.onload = function (evt) {
-            const code = evt.target.result;
-            alertShow("success", "Arquivo aberto");
-            addFile(file.path, code);
-        }
-        reader.onerror = function (evt) {
-            alertShow("error", "Erro ao ler o arquivo");
-        }
-        event.target.value = "";
-    }
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
+  const onChangeFile = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file, 'UTF-8');
+    reader.onload = function (evt) {
+      const code = evt.target.result;
+      alertShow('success', 'Arquivo aberto');
+      addFile(file.path, code);
     };
-
-    const handleClose = () => {
-        setAnchorEl(null);
+    reader.onerror = function (evt) {
+      alertShow('error', 'Erro ao ler o arquivo');
     };
+    event.target.value = '';
+  };
 
-    const handleAdd = () => {
-        addFile("", "", "Novo Arquivo");
-        setAnchorEl(null);
-    }
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleUpload = () => {
-        setAnchorEl(null);
-        ipcRenderer.send("invoke_open_file");
-        ipcRenderer.once("open_file", (e,path,code = "") => {
-            if(path === false){
-                if(code != ""){
-                    alertShow("error",code);
-                }
-            }else{
-                alertShow("success", "Arquivo aberto");
-                addFile(path,code);
-            }
-        });
-    }
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    return (
-        <>
-            <TabContainer
-                value={currentID || ""}
-                indicatorColor="secondary"
-                textColor="inherit"
-                onChange={onChange}
-                aria-label="disabled tabs example"
-                variant="scrollable"
-                scrollButtons="auto"
-                className={classes.root}
-                
-            >
-                
-                <Tab value="" onClick={handleClick} icon={<AddIcon />} className={classes.plus} />
-                {Object.entries(listFiles).map(([id, file], key) => <Item key={key} isSave={file.isSave} value={id} label={file.name} />)}
-            </TabContainer>
-            <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-            >
-                <MenuItem onClick={handleAdd}>Novo Arquivo</MenuItem>
-                <MenuItem onClick={handleUpload}>Enviar Arquivo</MenuItem>
-            </Menu>
-            <input accept=".asm" type='file' id='file' ref={inputFile} style={{ display: 'none' }} onChange={onChangeFile} />
-        </>);
-}
+  const handleAdd = () => {
+    addFile('', '', 'Novo Arquivo');
+    setAnchorEl(null);
+  };
+
+  const handleUpload = () => {
+    setAnchorEl(null);
+    ipcRenderer.send('invoke_open_file');
+    ipcRenderer.once('open_file', (e, path, code = '') => {
+      if (path === false) {
+        if (code != '') {
+          alertShow('error', code);
+        }
+      } else {
+        alertShow('success', 'Arquivo aberto');
+        addFile(path, code);
+      }
+    });
+  };
+
+  return (
+    <>
+      <TabContainer
+        value={currentID || ''}
+        indicatorColor='secondary'
+        textColor='inherit'
+        onChange={onChange}
+        aria-label='disabled tabs example'
+        variant='scrollable'
+        scrollButtons='auto'
+        className={classes.root}
+      >
+        <Tab
+          value=''
+          onClick={handleClick}
+          icon={<AddIcon />}
+          className={classes.plus}
+        />
+        {Object.entries(listFiles).map(([id, file], key) => (
+          <Item key={key} isSave={file.isSave} value={id} label={file.name} />
+        ))}
+      </TabContainer>
+      <Menu
+        id='simple-menu'
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleAdd}>Novo Arquivo</MenuItem>
+        <MenuItem onClick={handleUpload}>Enviar Arquivo</MenuItem>
+      </Menu>
+      <input
+        accept='.asm'
+        type='file'
+        id='file'
+        ref={inputFile}
+        style={{ display: 'none' }}
+        onChange={onChangeFile}
+      />
+    </>
+  );
+};
 export default Tabs;
