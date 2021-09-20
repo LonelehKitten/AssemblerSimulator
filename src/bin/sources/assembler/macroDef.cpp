f92a9e702982cb4e785fffa7b0d5d146dbfef8ce
LonelehKitten <scarface.theminer@gmail.com>
2021-09-19 21:04:46 -0300

    trabalho do time B feito

--- src/bin/sources/assembler/macroDef.cpp
+++ src/bin/sources/assembler/macroDef.cpp
@@ -4,6 +4,7 @@ macroDef::macroDef(std::string name, std::vector<std::string> args)
 {
     this->name = name;
     this->args = args;
+    this->macroText = new std::vector<Semantic *>();
 }
 
 std::string macroDef::getName()
@@ -16,14 +17,14 @@ std::vector<std::string> macroDef::getArgs()
     return this->args;
 }
 
-std::vector<Semantic *> & macroDef::getText()
+std::vector<Semantic *> * macroDef::getText()
 {
     return this->macroText;
 }
 
 void macroDef::setText(Semantic * line)
 {
-    this->macroText.push_back(line);
+    this->macroText->emplace_back(line);
 }
 
 
