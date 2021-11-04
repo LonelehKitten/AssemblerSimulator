d4dbebf9f497f47feeac79310bf620790dbf8639
LonelehKitten <scarface.theminer@gmail.com>
2021-11-04 04:17:30 -0300

    processador de expressões

---
+++ src/bin/sources/assembler/ExpressionEvaluator.cpp
@@ -0,0 +1,27 @@
+#include "ExpressionEvaluator.h"
+
+ExpressionEvaluator::ExpressionEvaluator(Expression * expression) :
+    priority1(false),
+    priority2(false),
+    priority3(false),
+    priority4(false),
+    expression(expression)
+{
+
+}
+
+void ExpressionEvaluator::solvePriority1() {
+
+}
+
+void ExpressionEvaluator::solvePriority2() {
+
+}
+
+void ExpressionEvaluator::solvePriority3() {
+
+}
+
+void ExpressionEvaluator::solvePriority4() {
+    
+}
---
+++ src/bin/sources/assembler/ExpressionEvaluator.h
@@ -0,0 +1,49 @@
+#ifndef EXPRESSION_EVALUATOR_H
+#define EXPRESSION_EVALUATOR_H
+
+#include <vector>
+#include "../analyzer/Token.h"
+
+typedef std::vector<Token *> Expression;
+
+/*
+
+    PASSOS
+    1. procurar o parenteses mais interno
+    2. encontrar o limite do parenteses mais interno
+    3. resolver as operações conforme a prioridade
+        3.1 buscar operação
+        3.2 identificar operandos
+        3.3 se o operando for símbolo, resolvê-lo pela tabela (a + b)
+        3.4 se o operando for literal, traduzir para um literal processável (a + 2)
+        3.5 se o operando for um valor já resolvido, reutilizá-lo
+        3.6 efetuar operação
+        3.7 substituir os tokens envolvidos na operação por um pseudo-token que indique o valor resolvido
+    4. repetir o processo até que todos os parenteses estejam resolvidos
+
+
+    ORDEM DE PRIORIDADE
+    0. '(' ')'
+    1. 'not' '+'(unario) '-' (unário)
+    2. '*' '/' 'mod' 'and'
+    3. '+' '-' 'or' 'xor'
+    4. 'eq' 'ne' 'lt' 'le' 'gt' 'ge'
+
+*/
+class ExpressionEvaluator {
+    private:
+        bool priority1, priority2, priority3, priority4;
+        Expression * expression;
+
+        int parenthesesCount;
+
+        void solvePriority1();
+        void solvePriority2();
+        void solvePriority3();
+        void solvePriority4();
+
+    public:
+        ExpressionEvaluator(Expression * expression);
+};
+
+#endif /* EXPRESSION_EVALUATOR_H */
